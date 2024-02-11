4f13dd3af72ac302e1ccc8a05ef89627cbf32b62
chihiros <szrkw2ui2a@gmail.com>
2024-02-12 02:36:37 +0900

    update: index.ts

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,11 +1,1 @@
-{
-  chrome.action.onClicked.addListener((_tab) => {
-    chrome.storage.local.get("url").then(({ url }) => {
-      if (url) {
-        chrome.tabs.create({
-          url,
-        });
-      }
-    });
-  });
-}
+{ }
