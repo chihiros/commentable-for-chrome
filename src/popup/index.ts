aa2e7564149cd2edbd1ae45e24988c01a40ebc51
chihiros <szrkw2ui2a@gmail.com>
2024-02-15 23:42:35 +0900

    タブのアクティブ状態でメッセージを送信するように変更

--- src/popup/index.ts
+++ src/popup/index.ts
@@ -5,14 +5,15 @@ document.addEventListener('DOMContentLoaded', () => {
   const checkbox = document.querySelector('#bordered-checkbox-2');
   checkbox.addEventListener('change', function () {
     chrome.storage.local.set({ 'showQRCode': this.checked });
-    chrome.tabs.query({}, function (tabs) {
-      tabs.forEach(tab => {
-        chrome.tabs.sendMessage(tab.id, { showQRCode: checkbox.checked });
-      });
+    // 現在アクティブなタブにのみメッセージを送信
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      if (tabs.length > 0) {
+        chrome.tabs.sendMessage(tabs[0].id, { showQRCode: checkbox.checked });
+      }
     });
-
   });
 
+  // ストレージから現在の設定を読み込み、チェックボックスの状態を復元する
   chrome.storage.local.get('showQRCode', (data) => {
     checkbox.checked = !!data.showQRCode;
   });
