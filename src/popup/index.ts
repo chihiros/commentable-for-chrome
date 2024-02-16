9528008f46edb2c2e63bfa6f368966c528906b9f
chihiros <szrkw2ui2a@gmail.com>
2024-02-17 00:23:12 +0900

    popup/index.tsの変更: 明示的な型付けとundefinedのチェックを追加

--- src/popup/index.ts
+++ src/popup/index.ts
@@ -1,20 +1,20 @@
 import '../styles/index.css';
 import '../styles/tailwind.css';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const checkbox = document.querySelector('#bordered-checkbox-2');
-  checkbox.addEventListener('change', function () {
+document.addEventListener('DOMContentLoaded', (): void => {
+  const checkbox = document.querySelector('#bordered-checkbox-2') as HTMLInputElement; // 明示的な型付け
+  checkbox.addEventListener('change', function (): void {
     chrome.storage.local.set({ 'showQRCode': this.checked });
     // 現在アクティブなタブにのみメッセージを送信
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      if (tabs.length > 0) {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs): void => {
+      if (tabs.length > 0 && tabs[0].id !== undefined) { // undefined チェック追加
         chrome.tabs.sendMessage(tabs[0].id, { showQRCode: checkbox.checked });
       }
     });
   });
 
   // ストレージから現在の設定を読み込み、チェックボックスの状態を復元する
-  chrome.storage.local.get('showQRCode', (data) => {
+  chrome.storage.local.get('showQRCode', (data): void => {
     checkbox.checked = !!data.showQRCode;
   });
 });
